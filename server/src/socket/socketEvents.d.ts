e7f9dbfea4cfa749bf07f80c7279c6de6b795e17
kevinbrasileiro <kevin.brasileiro08@gmail.com>
2026-04-24 21:48:46 -0300

    fix: type typos :)

--- server/src/socket/socketEvents.d.ts
+++ server/src/socket/socketEvents.d.ts
@@ -1,18 +1,16 @@
-import type { Game, JoinGameRespose } from "../game/GamesManager.ts"
-import type { SubmitGuessRespone } from "../game/types.js"
-import type { GuessResult } from "../game/wordle.ts"
+import type { GameConfig, GameState, JoinGameResponse, SubmitGuessRespone } from "../game/types.js"
 
 export interface ServerToClientEvents {
   broadcast: (message: string) => void
   
-  update_game_state: (gameState: GameStateType) => void
+  update_game_state: (gameState: GameState) => void
 }
 
 export interface ClientToServerEvents {
   ping: () => void
 
-  create_game: (username: string, config: Game["config"], callback: (gameId: string) => void) => void
-  join_game: (gameId: string, username: string, password: string | null, callback: (res: JoinGameRespose) => void) => void
+  create_game: (username: string, config: GameConfig, callback: (gameId: string) => void) => void
+  join_game: (gameId: string, username: string, password: string | null, callback: (res: JoinGameResponse) => void) => void
 
   submit_guess: (guess: string, callback: (res: SubmitGuessRespone) => void) => void
   vote_rematch: () => void
